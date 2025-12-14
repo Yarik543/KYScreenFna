6257a9eff421ba59287caa49911007bf53bdef3c
Yarik543 <162128013+Yarik543@users.noreply.github.com>
2025-12-14 20:34:41 +0300

    Merge pull request #1 from Yarik543/feature
    PullFNA

---
+++ ScreenFna/Src/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using ScreenSaverFna;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenFna.Src
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (var game = new ScreenSaverGame())
+            {
+                game.Run();
+            }
+        }
+    }
+}
---
+++ ScreenFna/Src/Snowflake.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ScreenSaverFna
+{
+    public struct Snowflake
+    {
+        public Texture2D Texture;
+        public Vector2 Position;
+        public float Speed;
+        public float Scale;
+        public float Rotation;
+        public float RotationSpeed;
+        public float Layer;
+    }
+}
